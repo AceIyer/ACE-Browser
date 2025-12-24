32c25fcfb05715b7dc5c49c346df3a828737f92a
Ace <AceIyer92@gmail.com>
2025-12-24 12:28:53 +0200

    The site blocker and redirect blocker works well, i just dislike the fact that the blocker asks before they need to nmove to a legit site , maybe i can make changes

--- background.js
+++ background.js
@@ -13,3 +13,35 @@ chrome.runtime.onInstalled.addListener((details) => {
         });
     }
 });
+
+// opens the index.html when icon is clicked 
+// this should allow users to then add their own websites that wish to block 
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.create({
+        url:"Index.html"
+    });
+});
+
+// This should block all unwated redirects from websites
+
+chrome.tabs.onCreated.addListener((tab) => {
+    if(tab.openerTabId){
+
+        setTimeout(() => {
+            chrome.tabs.get(tab.id, (currentTab) => {
+                const url = currentTab.url || currentTab.pendingUrl || "";
+
+                //This should now not canel a new tab
+                //should fix the tab issue
+                if (url.startsWith("chrome://") || url.startsWith("edge://") || url === "about: blank"){
+                    return;
+                }
+                // This should cancel out pop-up that were forced
+                chrome.tabs.remove(tab.id);
+                console.log("Ace site blocker blocked : " + url);
+            });
+        }, 100);
+        
+    }
+});
--- content.js
+++ content.js
@@ -6,3 +6,31 @@ window.open = function(url, name, features){
 
     return null;
 };
+
+window.addEventListener('beforeunload', (event) => {
+    event.preventDefault();
+    event.returnValue = '';
+});
+
+const meta = document.querySelector('meta[http-equiv="refresh"]');
+if (meta) {
+    meta.remove();
+    console.log("Ace Blocker removed a hidden redirect")
+};
+
+// This should help between good and bad redirects
+let lastClickTime = 0;
+document.addEventListener('click', () => {
+    lastClickTime = Date.now();
+});
+
+window.open = function(url){
+    if(Date.now() - lastClickTime < 500){
+        return originalOpen(url);
+    }
+    else{
+        // This was maybe a forced redirect
+        console.log("Ace Blocked a forced redirect");
+        return null;
+    }
+};
